19d19963814dd73458ae256ea031571e215adeb9
Aurora <11296006+aurora_xuhy@user.noreply.gitee.com>
2022-07-10 15:49:03 +0000

    20220710

---
+++ 20220710.c
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{/*
+//	int f;
+//	int x;
+//	scanf("%d", &x);
+//	if (x < 0) {
+//		f = -1;
+//	}
+//	else if (x == 0) {
+//		f = 0;
+//	}
+//	else {
+//		f = 2 * x;
+//	}
+//	printf("%d", f);*/
+
+
+
+//if (grade >= 90) {
+//	printf("A");
+//}else if( grade>=80) {
+//	printf("B");
+//}else if( grade>=70) {
+//	printf("C");
+//}else if(grade>=60) {
+//	printf("D");
+//}else if(grade<60) {
+//	printf("E");
+//}
+//
+////int grade;
+////scanf("%d", &grade);
+////grade /= 10;
+////switch (grade) {
+////case 10:
+////case 9:
+////	printf("A\n");
+////	break;
+////case 8:
+////	printf("B\n");
+////	break;
+////case 7:
+////	printf("C\n");
+////	break;
+////case 6:
+////	printf("D\n");
+////	break;
+////default:
+////	printf("E\n");
+////	break;
+////
+////}
+
+	///*int n = 0;
+	//int x ;
+	//scanf("%d", &x);
+	//n++;
+	//x /= 10;
+	//while (x > 0) {
+	//	n++;
+	//	x /=10;
+	//}
+	//
+	//printf("%d", n);
+	   
+
+	//int n = 0; 
+	//int x;
+	//scanf("%d",&x);
+	//do {
+	//	x /= 10;
+	//	n++;
+
+	//} while (x > 0);
+	//printf("%d",n);
+	
+	
+	
+	return 0;
+
+
+
+}
